a3dcdda74cc254dedd75b569fe1569e062365afc
Vanishree32 <vanishree9964@gmail.com>
2022-03-20 14:41:40 +0530

    added division

--- src/com/calci/Calculator.java
+++ src/com/calci/Calculator.java
@@ -9,4 +9,12 @@ public class Calculator {
 	public int sub(int num1, int num2) {
 		return num1 - num2;
 	}
+
+	public int mul(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public int div(int num1, int num2) {
+		return num1 / num2;
+	}
 }
